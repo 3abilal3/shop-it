5ce26e5f85f8339491dd42e9cce61eb46418dbe5
3abilal3 <3abilal3@gmail.com>
2025-02-05 15:27:02 +0000

    updated

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.all("*", function(req, res) {
 	return apiResponse.notFoundResponse(res, "Page not found");
 });
 
-app.use((err, req, res) => {
+app.use((err, req, res)  => {
 	if(err.name  ==  "UnauthorizedError"){
 		return apiResponse.unauthorizedResponse(res,  err.message);
 	}
